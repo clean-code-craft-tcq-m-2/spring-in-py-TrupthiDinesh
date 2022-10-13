8f2a7458f9d69d17ae8d544457c196e746160a9a
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-10-13 20:16:31 +0530

    Update statistics.py
    used basic list ops instead of pandas

--- statistics.py
+++ statistics.py
@@ -1,8 +1,6 @@
-import pandas as pd
+
 def calculateStats(numbers):
-  df = pd.DataFrame(numbers, columns=['numbers'])
-  df = pd.DataFrame(data)
-  mean_df = df['numbers'].mean()
-  max_df = df['numbers'].max()
-  min_df = df['numbers'].min()
+  mean_df = sum(numbers)/len(numbers)
+  max_df = max(numbers)
+  min_df = min(numbers)
   return [mean_df,max_df,min_df]
