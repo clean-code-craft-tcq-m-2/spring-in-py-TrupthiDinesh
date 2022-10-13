c3e0ed5bee5d5d0d49da334a1939eb169f2c04ff
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-10-13 20:22:38 +0530

    Update statistics.py
    added null list input case

--- statistics.py
+++ statistics.py
@@ -1,6 +1,11 @@
 
 def calculateStats(numbers):
   computedStats = {}
+  if len(numbers) == 0:
+    computedStats["avg"] = nan
+    computedStats["max"] = nan
+    computedStats["min"] = nan
+  
   computedStats["avg"] = sum(numbers)/len(numbers)
   computedStats["max"] = max(numbers)
   computedStats["min"] = min(numbers)
